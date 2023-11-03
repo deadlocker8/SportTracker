6d802400f285c7443381b858d46bf67320eabf91
Robert Goldmann <deadlocker@gmx.de>
2023-11-03 23:51:14 +0100

    added new command line option to generate dummy data

--- src/SportTracker.py
+++ src/SportTracker.py
@@ -1,25 +1,28 @@
 import logging
 import os
+from datetime import datetime
 from typing import Any
 
 import click
 from TheCodeLabs_BaseUtils.DefaultLogger import DefaultLogger
 from TheCodeLabs_FlaskUtils import FlaskBaseApp
 from flask import Flask
+
 from blueprints import General, Authentication
 from logic import Constants
 from logic.UserService import UserService
-from logic.model.Models import db, User
+from logic.model.Models import db, User, Track, TrackType
 
 LOGGER = DefaultLogger().create_logger_if_not_exists(Constants.APP_NAME)
 
 
 class SportTracker(FlaskBaseApp):
-    def __init__(self, appName: str, rootDir: str, logger: logging.Logger, isDebug: bool = False):
+    def __init__(self, appName: str, rootDir: str, logger: logging.Logger, isDebug: bool, generateDummyData: bool):
         super().__init__(appName, rootDir, logger, serveFavicon=False)
 
         self._userService = UserService(self._settings['users'])
         self._isDebug = isDebug
+        self._generateDummyData = generateDummyData
 
         loggingSettings = self._settings['logging']
         if loggingSettings['enableRotatingLogFile']:
@@ -42,9 +45,20 @@ class SportTracker(FlaskBaseApp):
             db.create_all()
             self.__create_users(db)
 
+            if self._generateDummyData:
+                self.__create_dummy_data(db)
+
         @app.context_processor
         def inject_version_name() -> dict[str, Any]:
             return {'versionName': self._version['name']}
+
+        def format_duration(value):
+            hours = value // 3600
+            minutes = value % 3600 // 60
+
+            return f'{hours}:{str(minutes).zfill(2)}'
+
+        app.add_template_filter(format_duration)
 
         return app
 
@@ -53,10 +67,30 @@ class SportTracker(FlaskBaseApp):
             existingUser = User.query.filter_by(username=username).first()
             if existingUser is None:
                 LOGGER.debug(f'Creating missing user "{username}"')
-                user = User()
-                user.username = username
+                user = User(username=username)
                 database.session.add(user)
                 database.session.commit()
+
+    def __create_dummy_data(self, database):
+        if Track.query.count() == 0:
+            LOGGER.debug('Creating dummy data...')
+
+            track = Track(type=TrackType.BICYCLE,
+                          name='Short after work',
+                          startTime=datetime(year=2023, month=11, day=3, hour=12, minute=15, second=48),
+                          duration=60 * 35, distance=1000 * 15, user_id=1)
+            database.session.add(track)
+            track = Track(type=TrackType.BICYCLE,
+                          name='Normal One',
+                          startTime=datetime(year=2023, month=10, day=15, hour=18, minute=23, second=12),
+                          duration=60 * 67, distance=1000 * 31, user_id=1)
+            database.session.add(track)
+            track = Track(type=TrackType.BICYCLE,
+                          name='Longest tour I\'ve ever made and was quite interesting',
+                          startTime=datetime(year=2023, month=10, day=28, hour=19, minute=30, second=41),
+                          duration=60 * 93, distance=1000 * 42.2, user_id=1)
+            database.session.add(track)
+            database.session.commit()
 
     def _register_blueprints(self, app):
         app.register_blueprint(Authentication.construct_blueprint(self._userService))
@@ -65,8 +99,9 @@ class SportTracker(FlaskBaseApp):
 
 @click.command()
 @click.option('--debug', '-d', is_flag=True, help="Enable debug mode")
-def start(debug):
-    server = SportTracker(Constants.APP_NAME, os.path.dirname(__file__), LOGGER, debug)
+@click.option('--dummy', '-dummy', is_flag=True, help="Generate dummy tracks")
+def start(debug, dummy):
+    server = SportTracker(Constants.APP_NAME, os.path.dirname(__file__), LOGGER, debug, dummy)
     server.start_server()
 
 
