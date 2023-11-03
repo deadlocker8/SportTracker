99d5174694c9af29090f5971436c144b8ed8d0df
Robert Goldmann <deadlocker@gmx.de>
2023-11-03 20:00:40 +0100

    use lower case user names

--- src/blueprints/Authentication.py
+++ src/blueprints/Authentication.py
@@ -3,23 +3,29 @@ from flask import Blueprint, render_template, redirect, url_for, request, sessio
 from logic.UserService import UserService
 
 
-def construct_blueprint(userService: UserService):
-    USERNAME = 'r'
-
+def construct_blueprint(versionName: str, userService: UserService):
     authentication = Blueprint('authentication', __name__)
 
     @authentication.route('/login')
     def login():
-        return render_template('login.jinja2')
+        return render_template('login.jinja2', versionName=versionName)
 
     @authentication.route('/login', methods=['POST'])
     def login_post():
+        username = request.form.get('username')
         password = request.form.get('password')
 
-        expectedPassword = userService.get_password_by_username(USERNAME)
+        if username is None:
+            return render_template('login.jinja2', message='Unbekannter Nutzer')
+
+        username = username.strip().lower()
+        if not userService.has_user(username):
+            return render_template('login.jinja2', message='Unbekannter Nutzer')
+
         if password is None:
             return render_template('login.jinja2', message='Error parameter "password"!')
 
+        expectedPassword = userService.get_password_by_username(username)
         if password != expectedPassword:
             return render_template('login.jinja2', message='Falsches Passwort')
 
