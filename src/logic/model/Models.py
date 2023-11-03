497d6a7df01ed07a1bdc6d3aa6281f6945d04aa1
Robert Goldmann <deadlocker@gmx.de>
2023-11-03 23:48:28 +0100

    add track type enum

--- src/logic/model/Models.py
+++ src/logic/model/Models.py
@@ -1,3 +1,5 @@
+import enum
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Integer, DateTime, String
 from sqlalchemy.orm import Mapped, mapped_column
@@ -11,8 +13,21 @@ class User(db.Model):
     tracks = db.relationship('Track', backref='user', lazy=True)
 
 
+class TrackType(enum.Enum):
+    BICYCLE = 'BICYCLE', 'directions_bike', 'bg-warning'
+
+    def __new__(cls, name: str, icon: str, background_color: str):
+        member = object.__new__(cls)
+        member._value_ = name
+        member.icon = icon
+        member.background_color = background_color
+        return member
+
+
 class Track(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    type = db.Column(db.Enum(TrackType))
+    name: Mapped[String] = mapped_column(String, nullable=False)
     startTime: Mapped[DateTime] = mapped_column(DateTime, nullable=False)
     duration: Mapped[int] = mapped_column(Integer, nullable=False)
     distance: Mapped[int] = mapped_column(Integer, nullable=False)
