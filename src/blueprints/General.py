bbdd4a3d7235e8704862b158efb36f86c2e3e835
Robert Goldmann <deadlocker@gmx.de>
2023-11-03 23:51:23 +0100

    show tracks on index page

--- src/blueprints/General.py
+++ src/blueprints/General.py
@@ -1,8 +1,9 @@
 from TheCodeLabs_BaseUtils.DefaultLogger import DefaultLogger
 from TheCodeLabs_FlaskUtils.auth.SessionLoginWrapper import require_login
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
 
 from logic import Constants
+from logic.model.Models import Track, User
 
 LOGGER = DefaultLogger().create_logger_if_not_exists(Constants.APP_NAME)
 
@@ -13,6 +14,7 @@ def construct_blueprint():
     @general.route('/')
     @require_login
     def index():
-        return render_template('index.jinja2')
+        tracks = Track.query.join(User).filter(User.username == session['username']).all()
+        return render_template('index.jinja2', tracks=tracks)
 
     return general
