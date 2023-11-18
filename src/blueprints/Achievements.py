1739ce8aac70784b317f12657856e478af06a7a8
Robert Goldmann <deadlocker@gmx.de>
2023-11-18 12:54:48 +0100

    achievements: calculate real values for longest track and total distance

--- src/blueprints/Achievements.py
+++ src/blueprints/Achievements.py
@@ -1,9 +1,11 @@
 import logging
 
 from flask import Blueprint, render_template
-from flask_login import login_required
+from flask_login import login_required, current_user
+from sqlalchemy import func
 
 from logic import Constants
+from logic.model.Models import BikingTrack, db, RunningTrack
 
 LOGGER = logging.getLogger(Constants.APP_NAME)
 
@@ -14,6 +16,19 @@ def construct_blueprint():
     @achievements.route('/')
     @login_required
     def showAchievements():
-        return render_template('achievements.jinja2')
+        return render_template('achievements.jinja2',
+                               bikingLongestTrack=__get_longest_distance_by_type(BikingTrack) / 1000,
+                               bikingTotalDistance=__get_total_distance_by_type(BikingTrack) / 1000,
+                               runningLongestTrack=__get_longest_distance_by_type(RunningTrack) / 1000,
+                               runningTotalDistance=__get_total_distance_by_type(RunningTrack) / 1000
+                               )
+
+    def __get_longest_distance_by_type(trackClass) -> int:
+        return db.session.query(func.max(trackClass.distance)).filter(
+            trackClass.user_id == current_user.id).scalar() or 0
+
+    def __get_total_distance_by_type(trackClass) -> int:
+        return db.session.query(func.sum(trackClass.distance)).filter(
+            trackClass.user_id == current_user.id).scalar() or 0
 
     return achievements
