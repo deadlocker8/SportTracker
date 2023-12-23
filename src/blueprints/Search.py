19c65f92d92475780c9fcd05d00401e69b230ccf
Robert Goldmann <deadlocker@gmx.de>
2023-12-23 16:50:08 +0100

    #48 - added new page for search results

--- src/blueprints/Search.py
+++ src/blueprints/Search.py
@@ -1,9 +1,12 @@
 import logging
+from itertools import groupby
 
-from flask import Blueprint, render_template
-from flask_login import login_required
+from flask import Blueprint, render_template, request
+from flask_login import login_required, current_user
 
 from logic import Constants
+from logic.model.Track import Track
+from logic.model.User import User
 
 LOGGER = logging.getLogger(Constants.APP_NAME)
 
@@ -14,6 +17,21 @@ def construct_blueprint():
     @search.route('/search', methods=['POST'])
     @login_required
     def performSearch():
-        return render_template('search.jinja2', results=[])
+        searchText = request.form.get('searchText')
+
+        if searchText is None:
+            return render_template('search.jinja2', results={})
+
+        searchText = searchText.strip()
+
+        tracks = (Track.query.join(User)
+                  .filter(User.username == current_user.username)
+                  .filter(Track.name.icontains(searchText))
+                  .order_by(Track.startTime.desc())
+                  .all())
+
+        results = {k: list(g) for k, g in groupby(tracks, key=lambda track: track.startTime.strftime('%B %Y'))}
+
+        return render_template('search.jinja2', results=results, searchText=searchText)
 
     return search
