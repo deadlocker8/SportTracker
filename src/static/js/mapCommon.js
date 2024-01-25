f9928a57fbf96daac32bea8aec2722bb3216f39e
Robert Goldmann <deadlocker@gmx.de>
2024-01-25 23:01:46 +0100

    #36 - localize map

--- src/static/js/mapCommon.js
+++ src/static/js/mapCommon.js
@@ -26,6 +26,9 @@ function initElevationChartSettings()
 
 function initMapBase()
 {
+    L.registerLocale('map_locale', map_locale);
+    L.setLocale('map_locale');
+
     let map = L.map('map', {
         attributionControl: false,
         minZoom: 6,
@@ -47,18 +50,18 @@ function initMapBase()
             position: 'topright'
         },
         plugins: [
-            "d3@7.8.4/dist/d3.min.js",
-            "@tmcw/togeojson@5.6.2/dist/togeojson.umd.js",
-            "leaflet-geometryutil@0.9.3/src/leaflet.geometryutil.js",
-            "leaflet-almostover@1.0.1/src/leaflet.almostover.js",
-            "@raruto/leaflet-elevation@2.5.1/dist/leaflet-elevation.min.css",
-            "@raruto/leaflet-elevation@2.5.1/dist/leaflet-elevation.min.js",
-            "@raruto/leaflet-elevation@2.5.1/libs/leaflet-gpxgroup.js",
+            'd3@7.8.4/dist/d3.min.js',
+            '@tmcw/togeojson@5.6.2/dist/togeojson.umd.js',
+            'leaflet-geometryutil@0.9.3/src/leaflet.geometryutil.js',
+            'leaflet-almostover@1.0.1/src/leaflet.almostover.js',
+            '@raruto/leaflet-elevation@2.5.1/dist/leaflet-elevation.min.css',
+            '@raruto/leaflet-elevation@2.5.1/dist/leaflet-elevation.min.js',
+            '@raruto/leaflet-elevation@2.5.1/libs/leaflet-gpxgroup.js',
         ]
     }).setView([51, 13], 6);
 
     let attributionControl = L.control.attribution({position: 'bottomleft'}).addTo(map);
-    attributionControl.setPrefix('<a href="https://leafletjs.com/">Leaflet</a>');
+    attributionControl.setPrefix('<a href="https://leafletjs.com/" > Leaflet < /a>');
 
     return map;
 }
