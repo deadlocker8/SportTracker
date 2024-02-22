b22727cab2648d313a7b8e3d1365039b418d3bb9
Robert Goldmann <deadlocker@gmx.de>
2024-02-22 23:52:04 +0100

    use headless firefox

--- tests/conftest.py
+++ tests/conftest.py
@@ -47,7 +47,7 @@ def server(app):
 
 @pytest.fixture
 def firefox_options(firefox_options):
-    # firefox_options.add_argument('--headless')
+    firefox_options.add_argument('--headless')
     return firefox_options
 
 
