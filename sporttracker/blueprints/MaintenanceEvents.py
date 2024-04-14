0fd8e1cb0ce2a904ccbb53ba6d6dbe53b2555511
Robert Goldmann <deadlocker@gmx.de>
2024-04-14 15:07:04 +0200

    #87 - list all events

--- sporttracker/blueprints/MaintenanceEvents.py
+++ sporttracker/blueprints/MaintenanceEvents.py
@@ -1,19 +1,36 @@
 import logging
+from dataclasses import dataclass
+from datetime import datetime, timedelta
 
 from flask import Blueprint, render_template
 from flask_login import login_required
 
 from sporttracker.logic import Constants
+from sporttracker.logic.model.Track import TrackType
 
 LOGGER = logging.getLogger(Constants.APP_NAME)
 
 
+@dataclass
+class MaintenanceEventModel:
+    eventDate: datetime
+    type: TrackType
+    description: str
+
+
 def construct_blueprint():
-    maintenanceEvents = Blueprint('maintenanceEvents', __name__, static_folder='static', url_prefix='/maintenanceEvents')
+    maintenanceEvents = Blueprint('maintenanceEvents', __name__, static_folder='static',
+                                  url_prefix='/maintenanceEvents')
 
     @maintenanceEvents.route('/')
     @login_required
     def listMaintenanceEvents():
-        return render_template('maintenanceEvents/maintenanceEvents.jinja2')
+        maintenanceEvents = [
+            MaintenanceEventModel(datetime.now(), TrackType.BIKING, 'chain oiled'),
+            MaintenanceEventModel(datetime.now() - timedelta(days=7), TrackType.BIKING, 'New break pads'),
+            MaintenanceEventModel(datetime.now() - timedelta(days=10), TrackType.BIKING, 'chain oiled'),
+        ]
+        return render_template('maintenanceEvents/maintenanceEvents.jinja2',
+                               maintenanceEvents=maintenanceEvents)
 
     return maintenanceEvents
