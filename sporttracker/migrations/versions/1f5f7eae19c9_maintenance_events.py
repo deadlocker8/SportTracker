e836c51fd98c71de6a5b6db6265c7d8fd333a849
Robert Goldmann <deadlocker@gmx.de>
2024-04-17 22:36:50 +0200

    fix database upgrade

--- sporttracker/migrations/versions/1f5f7eae19c9_maintenance_events.py
+++ sporttracker/migrations/versions/1f5f7eae19c9_maintenance_events.py
@@ -8,6 +8,8 @@ Create Date: 2024-04-14 17:41:58.614132
 
 import sqlalchemy as sa
 from alembic import op
+from sqlalchemy import Inspector
+from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision = '1f5f7eae19c9'
@@ -17,19 +19,23 @@ depends_on = None
 
 
 def upgrade():
-    op.create_table(
-        'maintenance_event',
-        sa.Column('id', sa.Integer(), nullable=False),
-        sa.Column('type', sa.Enum('BIKING', 'RUNNING', 'HIKING', name='tracktype'), nullable=True),
-        sa.Column('event_date', sa.DateTime(), nullable=False),
-        sa.Column('description', sa.String(), nullable=False),
-        sa.Column('user_id', sa.Integer(), nullable=False),
-        sa.ForeignKeyConstraint(
-            ['user_id'],
-            ['user.id'],
-        ),
-        sa.PrimaryKeyConstraint('id'),
-    )
+    inspector = Inspector.from_engine(op.get_bind().engine)
+    tableNames = inspector.get_table_names()
+
+    if 'maintenance_event' not in tableNames:
+        op.create_table(
+            'maintenance_event',
+            sa.Column('id', sa.Integer(), nullable=False),
+            sa.Column('type', postgresql.ENUM(name='tracktype', create_type=False), nullable=True),
+            sa.Column('event_date', sa.DateTime(), nullable=False),
+            sa.Column('description', sa.String(), nullable=False),
+            sa.Column('user_id', sa.Integer(), nullable=False),
+            sa.ForeignKeyConstraint(
+                ['user_id'],
+                ['user.id'],
+            ),
+            sa.PrimaryKeyConstraint('id'),
+        )
 
 
 def downgrade():
