bc50911bda1f6e2e6062950772ec866dfd6dfa58
Robert Goldmann <deadlocker@gmx.de>
2024-04-20 11:12:58 +0200

    #91 - add selenium tests

--- tests/blueprints/test_PlannedTours.py
+++ tests/blueprints/test_PlannedTours.py
@@ -19,41 +19,34 @@ def prepare_test_data(app):
         create_user(TEST_USERNAME, TEST_PASSWORD, False, Language.ENGLISH)
 
 
-class TestMaintenanceEvents(SeleniumTestBaseClass):
+class TestPlannedTours(SeleniumTestBaseClass):
     def __open_form(self, selenium):
-        selenium.get(self.build_url('/maintenanceEvents'))
+        selenium.get(self.build_url('/plannedTours'))
 
         selenium.find_element(By.TAG_NAME, 'h1').find_element(By.TAG_NAME, 'a').click()
         WebDriverWait(selenium, 5).until(
             expected_conditions.text_to_be_present_in_element(
-                (By.TAG_NAME, 'h1'), 'New Maintenance Event'
+                (By.TAG_NAME, 'h1'), 'New Planned Tour'
             )
         )
 
     def __open_edit_form(self, selenium):
-        selenium.get(self.build_url('/maintenanceEvents/edit/1'))
+        selenium.get(self.build_url('/plannedTours/edit/1'))
 
         WebDriverWait(selenium, 5).until(
             expected_conditions.text_to_be_present_in_element(
-                (By.TAG_NAME, 'h1'), 'Edit Maintenance Event'
+                (By.TAG_NAME, 'h1'), 'Edit Planned Tour'
             )
         )
 
     @staticmethod
-    def __fill_form(selenium, trackType, date, startTime, description):
-        select = Select(selenium.find_element(By.ID, 'maintenance-event-type'))
+    def __fill_form(selenium, trackType, name):
+        select = Select(selenium.find_element(By.ID, 'planned-tour-type'))
         select.select_by_visible_text(trackType.name.capitalize())
-        dateInput = selenium.find_element(By.ID, 'maintenance-event-date')
-        dateInput.clear()
-        dateInput.send_keys(date)
 
-        timeInput = selenium.find_element(By.ID, 'maintenance-event-time')
-        timeInput.clear()
-        timeInput.send_keys(startTime)
-
-        descriptionInput = selenium.find_element(By.ID, 'maintenance-event-description')
-        descriptionInput.clear()
-        descriptionInput.send_keys(description)
+        nameInput = selenium.find_element(By.ID, 'planned-tour-name')
+        nameInput.clear()
+        nameInput.send_keys(name)
 
     def __click_submit_button(self, selenium):
         button = selenium.find_element(By.CSS_SELECTOR, 'section form button')
@@ -61,83 +54,76 @@ class TestMaintenanceEvents(SeleniumTestBaseClass):
         time.sleep(1)
         button.click()
 
-    def test_add_event_valid(self, server, selenium: WebDriver):
+    def test_add_tour_valid(self, server, selenium: WebDriver):
         self.login(selenium)
         self.__open_form(selenium)
-        self.__fill_form(selenium, TrackType.BIKING, '2023-02-01', '15:30', 'chain oiled')
+        self.__fill_form(selenium, TrackType.BIKING, 'Awesome Tour')
         selenium.find_element(By.CSS_SELECTOR, 'section form button').click()
 
         WebDriverWait(selenium, 5).until(
-            expected_conditions.text_to_be_present_in_element((By.TAG_NAME, 'h1'), 'Maintenance')
+            expected_conditions.text_to_be_present_in_element((By.TAG_NAME, 'h1'), 'Planned Tours')
         )
 
         assert len(selenium.find_elements(By.CSS_SELECTOR, 'section .card')) == 1
 
-    def test_add_event_all_empty(self, server, selenium: WebDriver):
+    def test_add_tour_all_empty(self, server, selenium: WebDriver):
         self.login(selenium)
         self.__open_form(selenium)
-        self.__fill_form(selenium, TrackType.BIKING, '', '', '')
+        self.__fill_form(selenium, TrackType.BIKING, '')
         selenium.find_element(By.CSS_SELECTOR, 'section form button').click()
 
-        assert selenium.current_url.endswith('/maintenanceEvents/add')
+        assert selenium.current_url.endswith('/plannedTours/add')
 
     def test_quick_filter_only_show_activated_track_types(self, server, selenium: WebDriver):
         self.login(selenium)
         self.__open_form(selenium)
-        self.__fill_form(selenium, TrackType.BIKING, '2023-02-01', '15:30', 'chain oiled')
+        self.__fill_form(selenium, TrackType.BIKING, 'Awesome Tour')
         selenium.find_element(By.CSS_SELECTOR, 'section form button').click()
 
         WebDriverWait(selenium, 5).until(
-            expected_conditions.text_to_be_present_in_element((By.TAG_NAME, 'h1'), 'Maintenance')
+            expected_conditions.text_to_be_present_in_element((By.TAG_NAME, 'h1'), 'Planned Tours')
         )
 
         self.__open_form(selenium)
-        self.__fill_form(selenium, TrackType.RUNNING, '2023-02-01', '15:30', 'new shoes')
+        self.__fill_form(selenium, TrackType.RUNNING, 'Run away')
         selenium.find_element(By.CSS_SELECTOR, 'section form button').click()
 
         WebDriverWait(selenium, 5).until(
-            expected_conditions.text_to_be_present_in_element((By.TAG_NAME, 'h1'), 'Maintenance')
+            expected_conditions.text_to_be_present_in_element((By.TAG_NAME, 'h1'), 'Planned Tours')
         )
 
         selenium.find_elements(By.CLASS_NAME, 'quick-filter')[0].click()
 
         WebDriverWait(selenium, 5).until(
             expected_conditions.invisibility_of_element_located(
-                (By.XPATH, '//h4[text()="New Maintenance Event"]')
+                (By.XPATH, '//h4[text()="New Planned Tour"]')
             )
         )
 
         assert len(selenium.find_elements(By.CSS_SELECTOR, 'section .card')) == 1
 
-    def test_edit_event_valid(self, server, selenium: WebDriver, app):
+    def test_edit_tour_valid(self, server, selenium: WebDriver, app):
         self.login(selenium)
         self.__open_form(selenium)
-        self.__fill_form(selenium, TrackType.BIKING, '2023-02-01', '15:30', 'chain oiled')
+        self.__fill_form(selenium, TrackType.BIKING, 'Awesome Tour')
         selenium.find_element(By.CSS_SELECTOR, 'section form button').click()
 
         WebDriverWait(selenium, 5).until(
-            expected_conditions.text_to_be_present_in_element((By.TAG_NAME, 'h1'), 'Maintenance')
+            expected_conditions.text_to_be_present_in_element((By.TAG_NAME, 'h1'), 'Planned Tours')
         )
 
         self.__open_edit_form(selenium)
 
         assert (
-            selenium.find_element(By.ID, 'maintenance-event-date').get_attribute('value')
-            == '2023-02-01'
-        )
-        assert (
-            selenium.find_element(By.ID, 'maintenance-event-time').get_attribute('value') == '15:30'
-        )
-        assert (
-            selenium.find_element(By.ID, 'maintenance-event-description').get_attribute('value')
-            == 'chain oiled'
+            selenium.find_element(By.ID, 'planned-tour-name').get_attribute('value')
+            == 'Awesome Tour'
         )
 
-        self.__fill_form(selenium, TrackType.BIKING, '2023-02-02', '16:30', 'chain replaced')
+        self.__fill_form(selenium, TrackType.BIKING, 'Better Tour')
         selenium.find_element(By.CSS_SELECTOR, 'section form button[type="submit"]').click()
 
         WebDriverWait(selenium, 5).until(
-            expected_conditions.text_to_be_present_in_element((By.TAG_NAME, 'h1'), 'Maintenance')
+            expected_conditions.text_to_be_present_in_element((By.TAG_NAME, 'h1'), 'Planned Tours')
         )
 
-        assert len(selenium.find_elements(By.CLASS_NAME, 'maintenance-event-description')) == 1
+        assert len(selenium.find_elements(By.CLASS_NAME, 'planned-tour-name')) == 1
