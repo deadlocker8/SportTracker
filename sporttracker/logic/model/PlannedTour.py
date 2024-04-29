a6e103a8d2a8debdf29475ffd8d1856aa042a13c
Robert Goldmann <deadlocker@gmx.de>
2024-04-29 21:08:01 +0200

    #107 - add new properties for "arrival method" and "departure method" to planned tours

--- sporttracker/logic/model/PlannedTour.py
+++ sporttracker/logic/model/PlannedTour.py
@@ -1,3 +1,6 @@
+import enum
+
+from flask_babel import gettext
 from flask_login import current_user
 from sqlalchemy import Integer, DateTime, String, Table, Column, ForeignKey
 from sqlalchemy.orm import mapped_column, Mapped, relationship
@@ -7,6 +10,39 @@ from sporttracker.logic.DateTimeAccess import DateTimeAccess
 from sporttracker.logic.model.Track import TrackType
 from sporttracker.logic.model.User import User
 from sporttracker.logic.model.db import db
+
+
+class TravelType(enum.Enum):
+    NONE = 'NONE', 'home', 0
+    CAR = 'CAR', 'directions_car', 1
+    TRAIN = 'TRAIN', 'train', 2
+
+    icon: str
+    order: int
+
+    def __new__(
+        cls,
+        name: str,
+        icon: str,
+        order: int,
+    ):
+        member = object.__new__(cls)
+        member._value_ = name
+        member.icon = icon
+        member.order = order
+        return member
+
+    def get_localized_name(self) -> str:
+        # must be done this way to include translations in *.po and *.mo file
+        if self == self.NONE:
+            return gettext('None')
+        elif self == self.CAR:
+            return gettext('By Car')
+        elif self == self.TRAIN:
+            return gettext('By Train')
+
+        raise ValueError(f'Could not get localized name for unsupported TravelType: {self}')
+
 
 planned_tour_user_association = Table(
     'planned_tour_user_association',
@@ -24,6 +60,8 @@ class PlannedTour(db.Model, DateTimeAccess):  # type: ignore[name-defined]
     gpxFileName: Mapped[str] = mapped_column(String, nullable=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     shared_users: Mapped[list[User]] = relationship(secondary=planned_tour_user_association)
+    arrival_method = db.Column(db.Enum(TravelType))
+    departure_method = db.Column(db.Enum(TravelType))
 
     def __repr__(self):
         return (
@@ -35,6 +73,8 @@ class PlannedTour(db.Model, DateTimeAccess):  # type: ignore[name-defined]
             f'gpxFileName: {self.gpxFileName}, '
             f'user_id: {self.user_id}, '
             f'shared_users: {[user.id for user in self.shared_users]})'
+            f'arrival_method: {self.arrival_method})'
+            f'departure_method: {self.departure_method})'
         )
 
 
---
+++ sporttracker/migrations/versions/59f2cccf7d63_planned_tours_arrival_and_departure.py
@@ -0,0 +1,60 @@
+"""planned_tours_arrival_and_departure
+
+Revision ID: 59f2cccf7d63
+Revises: 4eebb86b3c7f
+Create Date: 2024-04-29 20:49:10.161647
+
+"""
+
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy import Inspector
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision = '59f2cccf7d63'
+down_revision = '4eebb86b3c7f'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    inspector = Inspector.from_engine(op.get_bind().engine)
+    columns = inspector.get_columns('planned_tour')
+    columnNames = [column['name'] for column in columns]
+
+    if 'arrival_method' not in columnNames:
+        op.add_column(
+            'planned_tour',
+            sa.Column(
+                'arrival_method',
+                postgresql.ENUM(name='traveltype', create_type=False),
+                nullable=True,
+                default='NONE',
+            ),
+        )
+
+    if 'departure_method' not in columnNames:
+        op.add_column(
+            'planned_tour',
+            sa.Column(
+                'departure_method',
+                postgresql.ENUM(name='traveltype', create_type=False),
+                nullable=True,
+                default='NONE',
+            ),
+        )
+
+    op.execute('UPDATE planned_tour SET arrival_method=\'NONE\' WHERE arrival_method IS NULL;')
+    op.execute('UPDATE planned_tour SET departure_method=\'NONE\' WHERE departure_method IS NULL;')
+
+
+def downgrade():
+    inspector = Inspector.from_engine(op.get_bind().engine)
+    columnNames = inspector.get_columns('planned_tour')
+
+    if 'arrival_method' in columnNames:
+        op.drop_column('planned_tour', 'arrival_method')
+
+    if 'departure_method' in columnNames:
+        op.drop_column('planned_tour', 'departure_method')
