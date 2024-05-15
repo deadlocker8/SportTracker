a0e3ceec5ab23c44f4fef886f38c5618edcc663f
Robert Goldmann <deadlocker@gmx.de>
2024-05-15 22:23:09 +0200

    #110 - show difference to previous year

--- sporttracker/blueprints/AnnualAchievements.py
+++ sporttracker/blueprints/AnnualAchievements.py
@@ -8,7 +8,7 @@ from flask_login import login_required
 from sporttracker.helpers import Helpers
 from sporttracker.logic import Constants
 from sporttracker.logic.AchievementCalculator import AchievementCalculator
-from sporttracker.logic.model.Achievement import Achievement
+from sporttracker.logic.model.Achievement import AnnualAchievement, AnnualAchievementDifferenceType
 from sporttracker.logic.model.Track import TrackType, get_available_years
 
 LOGGER = logging.getLogger(Constants.APP_NAME)
@@ -36,84 +36,120 @@ def construct_blueprint():
             availableYears=get_available_years(),
         )
 
-    def __get_annual_achievements(year: int) -> dict[TrackType, list[Achievement]]:
+    def __get_annual_achievements(year: int) -> dict[TrackType, list[AnnualAchievement]]:
         result = {}
 
         for trackType in TrackType:
             achievementList = []
+
+            totalDistance = AchievementCalculator.get_total_distance_by_type_and_year(
+                trackType, year
+            )
+            totalDistancePreviousYear = AchievementCalculator.get_total_distance_by_type_and_year(
+                trackType, year - 1
+            )
+            totalDistanceDifference = totalDistance - totalDistancePreviousYear
+
             achievementList.append(
-                Achievement(
+                AnnualAchievement(
                     icon='map',
                     is_font_awesome_icon=False,
                     color=trackType.border_color,
                     title=gettext('Total Distance'),
-                    description='{totalDistance} km'.format(
-                        totalDistance=Helpers.format_decimal(
-                            AchievementCalculator.get_total_distance_by_type_and_year(
-                                trackType, year
-                            ),
-                            decimals=2,
-                        )
+                    description=__format_distance(totalDistance),
+                    difference_to_previous_year=__format_distance(abs(totalDistanceDifference)),
+                    difference_type=AnnualAchievementDifferenceType.get_by_difference(
+                        totalDistanceDifference
                     ),
                 )
             )
+
+            totalDuration = AchievementCalculator.get_total_duration_by_type_and_year(
+                trackType, year
+            )
+            totalDurationPreviousYear = AchievementCalculator.get_total_duration_by_type_and_year(
+                trackType, year - 1
+            )
+            totalDurationDifference = totalDuration - totalDurationPreviousYear
+
             achievementList.append(
-                Achievement(
+                AnnualAchievement(
                     icon='timer',
                     is_font_awesome_icon=False,
                     color=trackType.border_color,
                     title=gettext('Total Duration'),
-                    description='{totalDuration} h'.format(
-                        totalDuration=Helpers.format_duration(
-                            AchievementCalculator.get_total_duration_by_type_and_year(
-                                trackType, year
-                            )
-                        )
+                    description=__format_duration(totalDuration),
+                    difference_to_previous_year=__format_duration(abs(totalDurationDifference)),
+                    difference_type=AnnualAchievementDifferenceType.get_by_difference(
+                        totalDurationDifference
                     ),
                 )
             )
+
+            totalTrackCount = AchievementCalculator.get_total_number_of_tracks_by_type_and_year(
+                trackType, year
+            )
+            totalTrackCountPreviousYear = (
+                AchievementCalculator.get_total_number_of_tracks_by_type_and_year(
+                    trackType, year - 1
+                )
+            )
+            totalTrackCountDifference = totalTrackCount - totalTrackCountPreviousYear
+
             achievementList.append(
-                Achievement(
+                AnnualAchievement(
                     icon='fa-route',
                     is_font_awesome_icon=True,
                     color=trackType.border_color,
                     title=gettext('Number Of Tracks'),
-                    description=str(
-                        AchievementCalculator.get_total_number_of_tracks_by_type_and_year(
-                            trackType, year
-                        )
+                    description=str(totalTrackCount),
+                    difference_to_previous_year=str(abs(totalTrackCountDifference)),
+                    difference_type=AnnualAchievementDifferenceType.get_by_difference(
+                        totalTrackCountDifference
                     ),
                 )
             )
+
+            longestTrack = AchievementCalculator.get_longest_distance_by_type_and_year(
+                trackType, year
+            )
+            longestTrackPreviousYear = AchievementCalculator.get_longest_distance_by_type_and_year(
+                trackType, year - 1
+            )
+            longestTrackDifference = longestTrack - longestTrackPreviousYear
+
             achievementList.append(
-                Achievement(
+                AnnualAchievement(
                     icon='route',
                     is_font_awesome_icon=False,
                     color=trackType.border_color,
                     title=gettext('Longest Track'),
-                    description='{longestDistance} km'.format(
-                        longestDistance=Helpers.format_decimal(
-                            AchievementCalculator.get_longest_distance_by_type_and_year(
-                                trackType, year
-                            ),
-                            decimals=2,
-                        )
+                    description=__format_distance(longestTrack),
+                    difference_to_previous_year=__format_distance(abs(longestTrackDifference)),
+                    difference_type=AnnualAchievementDifferenceType.get_by_difference(
+                        longestTrackDifference
                     ),
                 )
             )
+
+            averageSpeed = AchievementCalculator.get_longest_distance_by_type_and_year(
+                trackType, year
+            )
+            averageSpeedPreviousYear = AchievementCalculator.get_longest_distance_by_type_and_year(
+                trackType, year - 1
+            )
+            averageSpeedDifference = averageSpeed - averageSpeedPreviousYear
+
             achievementList.append(
-                Achievement(
+                AnnualAchievement(
                     icon='speed',
                     is_font_awesome_icon=False,
                     color=trackType.border_color,
                     title=gettext('Average Speed'),
-                    description='{averageSpeed} km/h'.format(
-                        averageSpeed=Helpers.format_decimal(
-                            AchievementCalculator.get_longest_distance_by_type_and_year(
-                                trackType, year
-                            ),
-                            decimals=2,
-                        )
+                    description=__format_speed(averageSpeed),
+                    difference_to_previous_year=__format_speed(abs(averageSpeedDifference)),
+                    difference_type=AnnualAchievementDifferenceType.get_by_difference(
+                        averageSpeedDifference
                     ),
                 )
             )
@@ -122,3 +158,15 @@ def construct_blueprint():
         return result
 
     return annualAchievements
+
+
+def __format_distance(distance: float) -> str:
+    return '{distance} km'.format(distance=Helpers.format_decimal(distance, decimals=2))
+
+
+def __format_duration(duration: int) -> str:
+    return '{duration} h'.format(duration=Helpers.format_duration(duration))
+
+
+def __format_speed(speed: float) -> str:
+    return '{speed} km/h'.format(speed=Helpers.format_decimal(speed, decimals=2))
--- sporttracker/logic/model/Achievement.py
+++ sporttracker/logic/model/Achievement.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+import enum
 from dataclasses import dataclass
 
 
@@ -8,3 +11,40 @@ class Achievement:
     color: str
     title: str
     description: str
+
+
+class AnnualAchievementDifferenceType(enum.Enum):
+    LESS = 'LESS', 'trending_down', 'text-danger'
+    EQUAL = 'EQUAL', 'trending_down', 'text-success'
+    MORE = 'MORE', 'trending_up', 'text-success'
+
+    icon: str
+    color: str
+
+    def __new__(
+        cls,
+        name: str,
+        icon: str,
+        color: str,
+    ):
+        member = object.__new__(cls)
+        member._value_ = name
+        member.icon = icon
+        member.color = color
+        return member
+
+    @staticmethod
+    def get_by_difference(difference: float) -> AnnualAchievementDifferenceType:
+        if difference < 0:
+            return AnnualAchievementDifferenceType.LESS
+
+        if difference == 0:
+            return AnnualAchievementDifferenceType.EQUAL
+
+        return AnnualAchievementDifferenceType.MORE
+
+
+@dataclass
+class AnnualAchievement(Achievement):
+    difference_to_previous_year: str
+    difference_type: AnnualAchievementDifferenceType
