cec37149f918bd9c8d3a782ca464744aa7752861
Robert Goldmann <deadlocker@gmx.de>
2024-12-31 17:55:44 +0100

    #153 - add new month goal type MonthGoalDuration

--- sporttracker/logic/model/MonthGoal.py
+++ sporttracker/logic/model/MonthGoal.py
@@ -8,6 +8,7 @@ from flask_login import current_user
 from sqlalchemy import Integer
 from sqlalchemy.orm import mapped_column, Mapped
 
+from sporttracker.helpers.Helpers import format_duration
 from sporttracker.logic.model.Track import get_tracks_by_year_and_month_by_type
 from sporttracker.logic.model.TrackType import TrackType
 from sporttracker.logic.model.User import User
@@ -58,6 +59,19 @@ class MonthGoalCountSummary(MonthGoalSummary):
 
     def get_actual_value_formatted(self) -> str:
         return f'{self.actual_count} / {self.goal_count_perfect}'
+
+
+@dataclass
+class MonthGoalDurationSummary(MonthGoalSummary):
+    type_name: ClassVar[str] = 'DURATION'
+    icon: ClassVar[str] = 'timer'
+
+    goal_duration_minimum: float
+    goal_duration_perfect: float
+    actual_duration: float
+
+    def get_actual_value_formatted(self) -> str:
+        return f'{format_duration(int(self.actual_duration))} h'
 
 
 class MonthGoal(db.Model):  # type: ignore[name-defined]
@@ -161,6 +175,52 @@ class MonthGoalCount(MonthGoal):
             f'month: {self.month}, '
             f'count_minimum: {self.count_minimum}, '
             f'count_perfect: {self.count_perfect}, '
+            f'user_id: {self.user_id})'
+        )
+
+
+class MonthGoalDuration(MonthGoal):
+    duration_minimum: Mapped[int] = mapped_column(Integer, nullable=False)
+    duration_perfect: Mapped[int] = mapped_column(Integer, nullable=False)
+
+    def get_summary(self) -> MonthGoalDurationSummary:
+        tracks = get_tracks_by_year_and_month_by_type(self.year, self.month, [self.type])
+
+        actualDuration = 0
+        if tracks:
+            actualDuration = sum([t.duration for t in tracks])
+
+        color = self.__determine_color(actualDuration)
+        name = format_datetime(date(year=self.year, month=self.month, day=1), format='MMMM yyyy')
+        percentage = actualDuration / self.duration_perfect * 100
+        return MonthGoalDurationSummary(
+            id=self.id,
+            type=self.type,
+            name=name,
+            goal_duration_minimum=self.duration_minimum,
+            goal_duration_perfect=self.duration_perfect,
+            actual_duration=actualDuration,
+            percentage=percentage,
+            color=color,
+        )
+
+    def __determine_color(self, actualDuration: int) -> str:
+        if actualDuration >= self.duration_perfect:
+            return 'bg-success'
+        elif actualDuration >= self.duration_minimum:
+            return 'bg-warning'
+
+        return 'bg-danger'
+
+    def __repr__(self):
+        return (
+            f'MonthGoalDuration('
+            f'id: {self.id}, '
+            f'type: {self.type}, '
+            f'year: {self.year}, '
+            f'month: {self.month}, '
+            f'duration_minimum: {self.duration_minimum}, '
+            f'duration_perfect: {self.duration_perfect}, '
             f'user_id: {self.user_id})'
         )
 
